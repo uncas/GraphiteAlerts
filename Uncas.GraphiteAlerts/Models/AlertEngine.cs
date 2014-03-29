5804d8c16c0f151ee48a89f93d9ddf11bcf676ce
Ole Lynge Soerensen <olelynge@gmail.com>
2014-03-29 07:46:26 +0100

    Include more rule logic in rule class.

--- Uncas.GraphiteAlerts/Models/AlertEngine.cs
+++ Uncas.GraphiteAlerts/Models/AlertEngine.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Uncas.GraphiteAlerts.Models.Graphite;
 
@@ -28,56 +27,14 @@ namespace Uncas.GraphiteAlerts.Models
             double newestValue = newest.Value.Value;
             foreach (AlertRule rule in alert.Rules)
             {
-                if (CheckRule(newestValue, rule))
+                if (rule.CheckRule(newestValue))
                     return new AlertResult(rule.Level, newestValue, newest.Timestamp,
-                        GetComment(rule, newestValue));
+                        rule.GetErrorComment(newestValue));
             }
 
             return new AlertResult(
                 AlertLevel.Ok, newestValue, newest.Timestamp,
                 string.Format("The actual value '{0:G4}' is OK.", newestValue));
-        }
-
-        private string GetComment(AlertRule rule, double newestValue)
-        {
-            string format = GetFormat(rule);
-            return string.Format(format, rule.Value, newestValue);
-        }
-
-        private static string GetFormat(AlertRule rule)
-        {
-            switch (rule.Operator)
-            {
-                case ">":
-                    return
-                        "The actual value '{1:G4}' is larger than the limit at '{0:G4}'.";
-                case "<":
-                    return
-                        "The actual value '{1:G4}' is smaller than the limit at '{0:G4}'.";
-            }
-
-            return string.Empty;
-        }
-
-        private bool CheckRule(double newestValue, AlertRule rule)
-        {
-            return GetRuleOperator(rule)(newestValue, rule.Value);
-        }
-
-        private Func<double, double, bool> GetRuleOperator(AlertRule rule)
-        {
-            switch (rule.Operator)
-            {
-                case ">":
-                    return (actualValue, limit) => actualValue > limit;
-                case "<":
-                    return (actualValue, limit) => actualValue < limit;
-                case "=":
-                case "==":
-                    return (actualValue, limit) => actualValue.Equals(limit);
-                default:
-                    return (actualValue, limit) => false;
-            }
         }
     }
 }
--- Uncas.GraphiteAlerts/Models/AlertRule.cs
+++ Uncas.GraphiteAlerts/Models/AlertRule.cs
@@ -1,4 +1,6 @@
-﻿namespace Uncas.GraphiteAlerts.Models
+﻿using System;
+
+namespace Uncas.GraphiteAlerts.Models
 {
     public class AlertRule
     {
@@ -12,5 +14,47 @@
         public string Operator { get; private set; }
         public double Value { get; private set; }
         public AlertLevel Level { get; private set; }
+
+        public bool CheckRule(double newestValue)
+        {
+            return GetOperatorFunc()(newestValue, Value);
+        }
+
+        public string GetErrorComment(double newestValue)
+        {
+            string format = GetErrorMessageFormat();
+            return string.Format(format, Value, newestValue);
+        }
+
+        private string GetErrorMessageFormat()
+        {
+            switch (Operator)
+            {
+                case ">":
+                    return
+                        "The actual value '{1:G4}' is larger than the limit at '{0:G4}'.";
+                case "<":
+                    return
+                        "The actual value '{1:G4}' is smaller than the limit at '{0:G4}'.";
+            }
+
+            return string.Empty;
+        }
+
+        private Func<double, double, bool> GetOperatorFunc()
+        {
+            switch (Operator)
+            {
+                case ">":
+                    return (actualValue, limit) => actualValue > limit;
+                case "<":
+                    return (actualValue, limit) => actualValue < limit;
+                case "=":
+                case "==":
+                    return (actualValue, limit) => actualValue.Equals(limit);
+                default:
+                    return (actualValue, limit) => false;
+            }
+        }
     }
 }
